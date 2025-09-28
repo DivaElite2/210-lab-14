8191683ae3677264fb81596f9570a2693d7fb636
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-09-28 11:03:18 -0700

    Class set up

---
+++ color.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Color {
+    private:
+    //color declaration
+    int red;
+    int green;
+    int blue;
+    public:
+    int getRedColor()  {return red; }
+    void setRed(int r) {red = r;}
+
+};
+
+int main(){
+
+}
