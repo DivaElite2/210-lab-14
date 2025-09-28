13ebce64db0a163a97d2a049c818b6ca92b7e434
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-09-28 11:14:40 -0700

    void print member function

--- color.cpp
+++ color.cpp
@@ -2,14 +2,30 @@
 using namespace std;
 
 class Color {
-    private:
+private:
     //color declaration
     int red;
     int green;
     int blue;
-    public:
+
+public:
+
     int getRedColor()  {return red; }
     void setRed(int r) {red = r;}
+
+    int getGreenColor()  {return green; }
+    void setGreen(int g) {green = g;}
+
+    int getBlueColor()  {return blue; }
+    void setBlue(int b) {blue = b;}
+   
+    //member print()
+   void print() {
+    cout << "Red: " << red << endl;
+    cout << "Green: " << green <<endl;
+    cout << "Blue: " << blue <<endl;
+   }
+
 
 };
 
